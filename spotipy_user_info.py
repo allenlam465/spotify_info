1241041283390a4c1ae1ff390adc11e2b0034d47
opt465 <allenlam465@gmail.com>
2019-01-29 23:15:23 -0800

    Adding top tracks to a playlist working.

--- spotipy_user_info.py
+++ spotipy_user_info.py
@@ -7,42 +7,46 @@ import spotipy.util as util
 
 from json.decoder import JSONDecodeError
 
- #Username: 1250747227
+#Username: 1250747227
 
 username = sys.argv[1]
 client_id = '23e4aa15bd7445eda0c9aaf36ebd7949'
 secret_id = 'd8b0eb0227ef41998f707cb0ef8e1b22'
 redirect_uri = 'http://localhost/'
+scope = 'user-top-read playlist-read-private user-library-read user-library-modify playlist-modify-public playlist-modify-private'
 
 try:
-    token = util.prompt_for_user_token(username,'user-top-read playlist-read-private user-library-read',client_id,secret_id,redirect_uri)
+    token = util.prompt_for_user_token(username, scope,client_id,secret_id,redirect_uri)
 except:
     os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username,'user-top-read playlist-read-private user-library-read',client_id,secret_id,redirect_uri)
+    token = util.prompt_for_user_token(username,scope,client_id,secret_id,redirect_uri)
 
 spotifyObj = spotipy.Spotify(auth=token)
 
 user = spotifyObj.current_user()
-top_artist = spotifyObj.current_user_top_artists(limit=100, offset=0, time_range='medium_term')
-top_tracks = spotifyObj.current_user_top_tracks(limit=50, offset=0)
+top_artist = spotifyObj.current_user_top_artists(limit=100, offset=0, time_range='long_term')
+top_tracks = spotifyObj.current_user_top_tracks(limit=50, offset=0, time_range='long_term')
+playlists = spotifyObj.current_user_playlists()
 
-#print (json.dumps(user, sort_keys=True, indent=4))
-#print (json.dumps(top_artist, sort_keys=True, indent=4))
-#print (json.dumps(top_tracks, sort_keys=True, indent=4))
+playlist_name = input("What is the playlist name? \n>")
+exist = False
 
-print("Popular artist in your saved song.")
-for artist in top_artist['items']:
-    print (artist['name'])
-    genres = []
-    for genre in artist['genres']:
-        genres.append(genre)
-    print (genres)   
+for pl in playlists['items']:
+    if pl['name'] == playlist_name:
+        exist = True
+        top_playlist = pl
+        break
 
-print("Your top 50 songs within your liked songs.")
-num = 1
+if not exist:
+    top_playlist = spotifyObj.user_playlist_create(username, playlist_name, public=False)
+else:
+    top_playlist = spotifyObj.user_playlist_create(username, "Your Top Tracks", public=False)
+
+track_list = []
 for track in top_tracks['items']:
-    print(num, '. ' + track['artists'][0]['name'] + ' - ' + track['name'])
-    num += 1
+    track_list.append(track['id'])
+
+results = spotifyObj.user_playlist_add_tracks(username, top_playlist['id'], track_list)
 
 
 
